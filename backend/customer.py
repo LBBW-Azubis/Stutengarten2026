312bbc903b22def7ab83de4de8d3b720e81579ac
Julian <108462647+jxlian005@users.noreply.github.com>
2025-08-22 13:19:51 +0200

    Add files via upload

---
+++ backend/customer.py
@@ -0,0 +1,151 @@
+from db_connector import DbConnector
+
+
+class CustomException(Exception):
+    pass
+
+
+class CustomerException(Exception):
+    pass
+
+
+class SavingsBookRef:
+    def __init__(self, customer_id, balance):
+        self.customer_id = customer_id
+        self.balance = balance
+
+
+class Customer:
+    def __init__(self, db: DbConnector, stutengarten_id=None, first_name=None, last_name=None, id=None):
+        self.db = db
+
+        if id is None:
+            # Validate required fields before insert
+            if stutengarten_id is None or first_name is None or last_name is None:
+                raise CustomerException("stutengarten_id, first_name and last_name must be set to create a new customer.")
+
+            # Create new customer in DB
+            conn = db.get_connection()
+            cursor = conn.cursor()
+            try:
+                cursor.execute(
+                    "INSERT INTO kunden (stutengarten_id, vorname, nachname) VALUES (%s, %s, %s)",
+                    (stutengarten_id, first_name, last_name),
+                )
+                self.id = cursor.lastrowid
+                self.stutengarten_id = stutengarten_id
+                self.first_name = first_name
+                self.last_name = last_name
+                conn.commit()
+            except Exception as err:
+                raise CustomException(f"Error creating customer: {err}")
+            finally:
+                cursor.close()
+        else:
+            # Customer object from existing data
+            self.id = id
+            self.stutengarten_id = stutengarten_id
+            self.first_name = first_name
+            self.last_name = last_name
+
+    def delete(self):
+        """
+        Delete this customer from the database.
+        """
+        conn = self.db.get_connection()
+        cursor = conn.cursor()
+        try:
+            cursor.execute("DELETE FROM kunden WHERE id = %s", (self.id,))
+            conn.commit()
+        finally:
+            cursor.close()
+
+    # Static methods
+    @staticmethod
+    def get_by_stutengarten_id(db: DbConnector, stutengarten_id):
+        conn = db.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        try:
+            cursor.execute("SELECT * FROM kunden WHERE stutengarten_id = %s", (stutengarten_id,))
+            row = cursor.fetchone()
+        finally:
+            cursor.close()
+        if row:
+            return Customer(db, row["stutengarten_id"], row["vorname"], row["nachname"], id=row["id"])
+        else:
+            raise CustomerException("No customer found!")
+
+    @staticmethod
+    def get_by_db_id(db: DbConnector, customer_id):
+        conn = db.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        try:
+            cursor.execute("SELECT * FROM kunden WHERE id = %s", (customer_id,))
+            row = cursor.fetchone()
+        finally:
+            cursor.close()
+        if row:
+            return Customer(db, row["stutengarten_id"], row["vorname"], row["nachname"], id=row["id"])
+        else:
+            raise CustomerException("No customer found!")
+
+    # Methods
+    def get_savings_book(self):
+        conn = self.db.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        try:
+            # First attempt: column name 'kunden'
+            try:
+                cursor.execute("SELECT * FROM sparbuecher WHERE kunden = %s", (self.id,))
+                row = cursor.fetchone()
+            except Exception:
+                # Fallback: if column 'kunden' doesn't exist, try 'kunden_id'
+                cursor.close()
+                cursor = conn.cursor(dictionary=True)
+                cursor.execute("SELECT * FROM sparbuecher WHERE kunden_id = %s", (self.id,))
+                row = cursor.fetchone()
+        finally:
+            cursor.close()
+        if row:
+            return SavingsBookRef(self.id, row["saldo"])
+        else:
+            raise CustomException("No savings book available")
+
+    # Update methods
+    def update_stutengarten_id(self, new_id):
+        conn = self.db.get_connection()
+        cursor = conn.cursor()
+        try:
+            cursor.execute("UPDATE kunden SET stutengarten_id = %s WHERE id = %s", (new_id, self.id))
+            conn.commit()
+            self.stutengarten_id = new_id
+        finally:
+            cursor.close()
+
+    def update_first_name(self, new_first_name):
+        conn = self.db.get_connection()
+        cursor = conn.cursor()
+        try:
+            cursor.execute("UPDATE kunden SET vorname = %s WHERE id = %s", (new_first_name, self.id))
+            conn.commit()
+            self.first_name = new_first_name
+        finally:
+            cursor.close()
+
+    def update_last_name(self, new_last_name):
+        conn = self.db.get_connection()
+        cursor = conn.cursor()
+        try:
+            cursor.execute("UPDATE kunden SET nachname = %s WHERE id = %s", (new_last_name, self.id))
+            conn.commit()
+            self.last_name = new_last_name
+        finally:
+            cursor.close()
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "stutengarten_id": self.stutengarten_id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+        }
