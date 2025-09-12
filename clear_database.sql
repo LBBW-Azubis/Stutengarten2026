3132acc7da855d891d52a3fad9b153289884f52f
Jannik <jannik@klys.de>
2025-09-12 15:09:30 +0200

    Create clear_database.sql

---
+++ clear_database.sql
@@ -0,0 +1,15 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- Alle Tabellen leeren:
+TRUNCATE TABLE kunden;
+TRUNCATE TABLE kundenaktien;
+TRUNCATE TABLE kundensparbuecher;
+TRUNCATE TABLE kundenumsaetze;
+TRUNCATE TABLE kundenstatistik;
+TRUNCATE TABLE unternehmen;
+TRUNCATE TABLE unternehmenssparbuecher;
+TRUNCATE TABLE unternehmensumsaetze;
+TRUNCATE TABLE unternehmensstatistik;
+TRUNCATE TABLE wirtschaftsbeihilfe;
+
+SET FOREIGN_KEY_CHECKS = 1;
