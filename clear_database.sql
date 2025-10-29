26132875338b64eca533b3991718b547b38fdd50
Julian <julian.jesinger@gmail.com>
2025-10-29 12:50:15 +0100

    Update clear_database.sql

--- clear_database.sql
+++ clear_database.sql
@@ -1,15 +1,21 @@
-SET FOREIGN_KEY_CHECKS = 0;
+-- 1. Datenbank auswählen
+USE stutengarten;
 
--- Alle Tabellen leeren:
-TRUNCATE TABLE kunden;
-TRUNCATE TABLE kundenaktien;
-TRUNCATE TABLE kundensparbuecher;
+-- 2. Fremdschlüsselprüfung ausschalten
+SET FOREIGN_KEY_CHECKS = 0; 
+
+-- 3. Alle Tabellen leeren (TRUNCATE ist schnell und setzt Auto-Increment-Zähler zurück)
 TRUNCATE TABLE kundenumsaetze;
-TRUNCATE TABLE kundenstatistik;
-TRUNCATE TABLE unternehmen;
-TRUNCATE TABLE unternehmenssparbuecher;
+TRUNCATE TABLE kundensparbuecher;
+TRUNCATE TABLE kundenaktien;
 TRUNCATE TABLE unternehmensumsaetze;
-TRUNCATE TABLE unternehmensstatistik;
+TRUNCATE TABLE unternehmenssparbuecher;
 TRUNCATE TABLE wirtschaftsbeihilfe;
+TRUNCATE TABLE kundenstatistik;
+TRUNCATE TABLE unternehmensstatistik;
+TRUNCATE TABLE kunden;
+TRUNCATE TABLE unternehmen;
+TRUNCATE TABLE aktien;
 
+-- 4. Fremdschlüsselprüfung wieder einschalten
 SET FOREIGN_KEY_CHECKS = 1;
